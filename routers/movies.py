c803345fbb0ce2a39e60d5e6efeafb5c18e44cfb
Mohamad Shaheen <mhmd.shahin@outlook.com>
2025-02-27 15:02:59 +0200

    finished movies router

--- routers/movies.py
+++ routers/movies.py
@@ -1,3 +1,5 @@
+import re
+from typing import List
 from db_access_layer import movies
 from fastapi import APIRouter, HTTPException
 
@@ -53,6 +55,79 @@ async def get_movies_with_popularity_over(popularity: float):
 async def get_movies_with_popularity_less(popularity: float):
     try:
         response = movies.get_movies_with_popularity_less(popularity)
+        return response
+    except HTTPException as err:
+        raise err
+
+
+@router.get("/movies-by-genre/")
+async def get_movies_by_genre(genre: str):
+    try:
+        response = movies.get_movies_by_genre(genre)
+        return response
+    except HTTPException as err:
+        raise err
+
+
+@router.get("/movies-released-after/")
+async def get_movies_released_after(year: int):
+    try:
+        response = movies.get_movies_released_after(year)
+        return response
+    except HTTPException as err:
+        raise err
+
+
+@router.get("/movies-released-before/")
+async def get_movies_released_before(year: int):
+    try:
+        response = movies.get_movies_released_before(year)
+        return response
+    except HTTPException as err:
+        raise err
+
+
+@router.post("/add-movies/")
+async def add_movies(movies_list: List[dict]):
+    try:
+        response = movies.add_movies(movies_list)
+        return response
+    except HTTPException as err:
+        raise err
+
+
+@router.post("/add-movie/")
+async def add_movie(id: int, title: str, language: str, popularity: float, release_date: str, adult: bool, genres: str,
+                    overview: str, poster_path: str):
+    try:
+        if not re.match(r"^\d{2}-\d{2}-\d{4}$", release_date):
+            raise HTTPException(status_code=400, detail="Invalid date format. Use dd-mm-yyyy")
+
+        if not re.match(r"^[A-Za-z]+(,[A-Za-z]+)*$", genres):
+            raise HTTPException(status_code=400,
+                                detail="Invalid genres format. Use comma-separated words like 'Action,Comedy'")
+
+        response = movies.add_movie(
+            {"id": id, "title": title, "language": language, "popularity": popularity, "release_date": release_date,
+             "adult": adult, "genres": genres, "overview": overview, "poster_path": poster_path, "actors": []})
+        return response
+    except HTTPException as err:
+        raise err
+
+
+@router.delete("/delete-movies/")
+async def delete_movies(movies_ids: List[int]):
+    try:
+        response = movies.delete_movies(movies_ids)
+        return response
+    except HTTPException as err:
+        raise err
+
+
+@router.delete("/delete-movie/")
+async def delete_movie(movie_id: int):
+    try:
+        response = movies.delete_movies(movie_id)
         return response
     except HTTPException as err:
         raise err
